1def739dde02b6dffa1cd9359e973282fb105f36
Mayur Mori <mayur.mori1994@gmail.com>
2020-04-03 10:33:17 +0530

    initial commit

---
+++ UIStackView/ViewController.swift
@@ -0,0 +1,11 @@
+//
+//  ViewController.swift
+//  UIStackView
+//
+//  Created by Mayur Mori on 14/09/19.
+//  Copyright © 2019 Mayur Mori. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {}
